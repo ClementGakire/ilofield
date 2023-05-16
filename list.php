78a5829bb1ccafa253b6df7a15b7d16dfa9fb2df
ClementGakire <clementgakire@gmail.com>
2023-05-16 08:47:35 +0200

    ...

--- list.php
+++ list.php
@@ -23,72 +23,10 @@ $fetchdata = new DB_con();
   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="css/owl.carousel.css">
   <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
-  <script nonce="2b08652c-5a00-46de-b293-4fb0d7c3a425">
-    
-    (function(w, d) {
-      ! function(dk, dl, dm, dn) {
-        dk[dm] = dk[dm] || {};
-        dk[dm].executed = [];
-        dk.zaraz = {
-          deferred: [],
-          listeners: []
-        };
-        dk.zaraz.q = [];
-        dk.zaraz._f = function(dp) {
-          return function() {
-            var dq = Array.prototype.slice.call(arguments);
-            dk.zaraz.q.push({
-              m: dp,
-              a: dq
-            })
-          }
-        };
-        for (const dr of ["track", "set", "debug"]) dk.zaraz[dr] = dk.zaraz._f(dr);
-        dk.zaraz.init = () => {
-          var ds = dl.getElementsByTagName(dn)[0],
-            dt = dl.createElement(dn),
-            du = dl.getElementsByTagName("title")[0];
-          du && (dk[dm].t = dl.getElementsByTagName("title")[0].text);
-          dk[dm].x = Math.random();
-          dk[dm].w = dk.screen.width;
-          dk[dm].h = dk.screen.height;
-          dk[dm].j = dk.innerHeight;
-          dk[dm].e = dk.innerWidth;
-          dk[dm].l = dk.location.href;
-          dk[dm].r = dl.referrer;
-          dk[dm].k = dk.screen.colorDepth;
-          dk[dm].n = dl.characterSet;
-          dk[dm].o = (new Date).getTimezoneOffset();
-          if (dk.dataLayer)
-            for (const dy of Object.entries(Object.entries(dataLayer).reduce(((dz, dA) => ({
-                ...dz[1],
-                ...dA[1]
-              }))))) zaraz.set(dy[0], dy[1], {
-              scope: "page"
-            });
-          dk[dm].q = [];
-          for (; dk.zaraz.q.length;) {
-            const dB = dk.zaraz.q.shift();
-            dk[dm].q.push(dB)
-          }
-          dt.defer = !0;
-          for (const dC of [localStorage, sessionStorage]) Object.keys(dC || {}).filter((dE => dE.startsWith("_zaraz_"))).forEach((dD => {
-            try {
-              dk[dm]["z_" + dD.slice(7)] = JSON.parse(dC.getItem(dD))
-            } catch {
-              dk[dm]["z_" + dD.slice(7)] = dC.getItem(dD)
-            }
-          }));
-          dt.referrerPolicy = "origin";
-          dt.src = "../../cdn-cgi/zaraz/sd0d9.js?z=" + btoa(encodeURIComponent(JSON.stringify(dk[dm])));
-          ds.parentNode.insertBefore(dt, ds)
-        };
-        ["complete", "interactive"].includes(dl.readyState) ? zaraz.init() : dk.addEventListener("DOMContentLoaded", zaraz.init)
-      }(w, d, "zarazData", "script");
-    })(window, document);
-  </script>
+  
+  
 </head>
 <body>
   <header id="header" id="home">
@@ -149,7 +87,7 @@ $fetchdata = new DB_con();
         </div>
         <div class="col-md-8 pb-40 header-text" style="padding-top: 35px !important">
         <h1>Side Event</h1>
-        <table id="example" class="display nowrap" style="width:100%">
+        <table id="side" class="display nowrap" style="width:100%">
         <thead>
             <tr>
                 <th>Name</th>
@@ -192,17 +130,30 @@ $fetchdata = new DB_con();
       </div>
     </div>
   </footer> 
-  <script async src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script async src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-  <script async src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-  <script async src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script async src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script async src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script async src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-  <script async src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.1/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/vfs_fonts.js"></script>
   <script>
     $(document).ready(function() {
     $('#example').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
+            lengthChange: true,
+            lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+            pageLength: 25,
+        });
+    });
+  </script>
+  <script>
+    $(document).ready(function() {
+    $('#side').DataTable({
             dom: 'Bfrtip',
             buttons: [
                 'copy', 'csv', 'excel', 'pdf', 'print'
